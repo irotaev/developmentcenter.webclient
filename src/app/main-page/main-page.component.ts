e646bc97403119a4d736858ce09fbb1f88f5782d
irotaev <irotaev@gmaill.com>
2019-08-08 15:45:38 +0300

    dev

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as ElementQueries from 'css-element-queries/src/ElementQueries';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+    (<any>ElementQueries).init();
   }
 
+  ngAfterViewInit() {
+    
+  }
 }
